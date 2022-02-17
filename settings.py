4bef40f76ba0fc6bcd2b8389f2ae63058f7c2faa
unknown <cezarylasota777@gmail.com>
2022-02-17 22:28:31 +0100

    first commit

---
+++ settings.py
@@ -0,0 +1,15 @@
+class Settings:
+    def __init__(self):
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (108, 242, 242)
+        self.ship_speed = 1.5
+        self.bullet_speed = 1.5
+        self.bullet_width = 10
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 20
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        self.fleet_direction = 1
+        self.ship_limit = 3
